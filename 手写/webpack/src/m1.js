d435c0bf3d84f1e743cd64f98b6c24ea3dc2379a
XJ <1163464003@qq.com>
2023-02-22 23:21:23 +0800

    Revert "feat：webpack"
    This reverts commit 014827c835f49aa25c95d0869e050c0f642bdc2e.

--- 手写/webpack/src/m1.js
+++ 手写/webpack/src/m1.js
@@ -1,11 +1,8 @@
-// import $ from 'jquery'
-import './assets/style/css/index.css';
-import flowers from './assets/img/1.jpg';
+import $ from 'jquery'
 
 export default {
 	setH1() {
 		// $('body').append('<h1>你好</h1>');
-		document.body.insertAdjacentHTML('beforeend', '<h1>你好11111111</h1>');
-		document.body.insertAdjacentHTML('beforeend', `<img src="${flowers}" />`);
-	},
-};
+		document.body.insertAdjacentHTML('beforeend', '<h1>你好</h1>');
+	}
+}
