f26929bde29c85bffdfd401970718dcd285a621c
XJ <1163464003@qq.com>
2023-02-22 23:22:27 +0800

    Revert "Revert "feat：webpack""
    This reverts commit d435c0bf3d84f1e743cd64f98b6c24ea3dc2379a.

--- 手写/webpack/src/index.js
+++ 手写/webpack/src/index.js
@@ -1,5 +1,9 @@
-import m1 from './m1.js'
-import m2 from './m2.js'
+import m1 from './m1.js';
+import m2 from './m2.js';
 
-m1.setH1()
-m2.sayHello()
+m1.setH1();
+m2.sayHello();
+document.body.insertAdjacentHTML('beforeend', '<h1>你好</h1>');
+document.body.onclick = () => {
+	alert('11111');
+};
